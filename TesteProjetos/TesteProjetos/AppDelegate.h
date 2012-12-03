6befd05079939f18a01b601c45c665b2510a7708
ViniciusScorsatto <vinicius.scorsatto@gmail.com>
2012-12-03 17:07:14 -0500

    ProjetoTeste

---
+++ TesteProjetos/TesteProjetos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TesteProjetos
+//
+//  Created by Anderson Spinelli on 12/3/12.
+//  Copyright (c) 2012 2be!creative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TesteProjetos/TesteProjetos/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TesteProjetos
+//
+//  Created by Anderson Spinelli on 12/3/12.
+//  Copyright (c) 2012 2be!creative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
